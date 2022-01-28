a340f9d5637f26658304613a42057ab4e894154c
Ritikramlogic <94177557+Ritikramlogic@users.noreply.github.com>
2022-01-28 19:32:23 +0530

    Create ETH20_to_Hertz.sol

---
+++ ETH20_to_Hertz.sol
@@ -0,0 +1,157 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-07-09
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SafeMath {
+ 
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+interface IBEP20 {
+   
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract ETH20_to_HTZ {
+    using SafeMath for uint256;
+    IBEP20 Hertz_token;
+    address private ContractAddress;
+    address private adminAddress;
+
+     constructor(address _ContractAddress,address _adminAddress) {
+        Hertz_token = IBEP20(_ContractAddress);
+        ContractAddress = _ContractAddress;
+        adminAddress = _adminAddress;
+        }
+        
+    function ETH20_Swap(uint256 ammount) public {
+        Hertz_token.transferFrom(msg.sender,adminAddress,ammount);
+    }
+
+    function checkAdminAddress() public view  virtual returns(address) {
+            return adminAddress;
+                }
+
+    function checkContractAddress() public view  virtual returns(address) {
+                return ContractAddress;
+            }
+        
+    function changeContractAddress(address _ContractAddress) public virtual {
+            ContractAddress = _ContractAddress;
+        }
+
+    function changeAdminAddress(address _adminAddress) public virtual {
+            adminAddress = _adminAddress;
+        }
+}
+
